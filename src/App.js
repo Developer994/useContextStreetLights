83dc63e71ff62eb0633d6fc34fb536743f0a5306
Rezwan Atcha <rez_a@outlook.com>
2023-04-09 13:03:49 -0400

    Added a button that starts the light, as opposed to the lights starting on their own.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,21 @@ export const ThemeContext = React.createContext();
 
 function App() {
   const [lightSwitch, setLightSwitch] = useState(true);
-  // const [vertlightSwitch, setVertLightSwitch] = useState(true); // Top and bottom light switches
-  // const [horlightSwitch, setHorLightSwitch] = useState(false); // Left and right light switches
 
   const toggleLight = () => {
     setLightSwitch((prevLightStatus) => !prevLightStatus);
-    // setVertLightSwitch((prevVertLightStatus) => !prevVertLightStatus);
-    // setHorLightSwitch((prevHorLightStatus) => !prevHorLightStatus);
   };
 
-  // useEffect(() => {
-  //   setTimeout(toggleLight, 3000);
-  // });
+  function LightHandler() {
+    useEffect(() => {
+      setTimeout(toggleLight, 3000);
+    });
+  }
 
   return (
     <>
       <ThemeContext.Provider value={lightSwitch}>
+        <button onClick={LightHandler()}>Toggle</button>
         <TrafficLightsVert />
         <TrafficLightsHori />
       </ThemeContext.Provider>
